0e0e2eaee8d1a9a08b5d8f3b37102369e0689957
Kateknm <31115258+Kateknm@users.noreply.github.com>
2019-01-10 22:13:37 -0800

    NavX added

--- subsystems/NavX_subsystem.java
+++ subsystems/NavX_subsystem.java
@@ -7,18 +7,67 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * Add your docs here.
+ * Author: Katie Markham
+ * Setting up NavX (gyro) subsystem  
+ *  
+ * https://www.kauailabs.com/public_files/navx-mxp
+ * /apidocs/java/com/kauailabs/navx/frc/AHRS.html
+ * 
+ * instructions for installing library:
+ * https://pdocs.kauailabs.com/navx-mxp/software/roborio-libraries/java/
  */
-public class NavX_subsystem extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
 
-  @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
-  }
+//TODO: Make NavX Commands, Add to Robot.java
+//TODO: Test Sensor
+
+//TODO: Test System
+public class NavX_subsystem extends Subsystem {
+	private AHRS ahrs;
+	
+	//constructor
+	public NavX_subsystem(){
+		try {
+			ahrs = new AHRS(SPI.Port.kMXP); //setting up SPI port for navx MXP
+			ahrs.enableLogging(true);
+		}
+		catch (RuntimeException ex) {
+			DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+		}
+	}
+	
+	//gets angle as a float -180 to 180
+	public float getAngle(){
+		return ahrs.getYaw();
+	}
+	
+	public void reset() {
+        ahrs.reset();
+    }
+
+    public void zeroYaw() {
+        ahrs.zeroYaw();
+    }
+	
+	public void getNavxData(){
+		//gets the values back from navx 
+		SmartDashboard.putBoolean("IMU_Connected", ahrs.isConnected());
+		SmartDashboard.putBoolean("IMU_IsCalibrating", ahrs.isCalibrating());
+		SmartDashboard.putNumber("IMU_YawAngle", ahrs.getYaw());
+		SmartDashboard.putNumber("IMU_Pitch", ahrs.getPitch());
+		SmartDashboard.putNumber("IMU_Roll", ahrs.getRoll());
+		//Fetches 6 Axis Data from MXP and prints to SmartDash
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+
 }
